25ced262b5649b0e829c20fc8fdf0591e8f23dd5
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-08-10 18:11:10 -0400

    added table migrations, controllers, models

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  resources :users
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+posts = { {
+    subject: "blah",
+    content: "blah blah blah", 
+    img_url: "",
+    bittersweet: "bitter"
+}, 
+{
+    subject: "bloop",
+    content: "bloop bloop bloop", 
+    img_url: "",
+    bittersweet: "bitter"
+},
+{
+    subject: "bleep",
+    content: "blah blah blah", 
+    img_url: "",
+    bittersweet: "bitter"
+}
+}
+
+users = {
+    username: "bob",
+    email: "bob@bob.com",
+    password: "bobob"
+}
