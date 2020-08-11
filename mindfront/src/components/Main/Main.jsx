be7770c6b36e32f6da0c8b5cb0f80187fcb551e4
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-08-11 16:59:44 -0400

    set up components folder and services files

---
+++ mindfront/src/components/Main/Main.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import { Route } from 'react-router-dom'
+//import { readAllPosts } from '../services/posts'
+//import { readAllComments } from '../services/comments'
+
+//import Login from './Login/Login'
+// import Subscribe from './Subscribe/Subscribe'
+// import CreatePost from './CreatePost/CreatePost'
+// import EditPost from './EditPost/EditPost'
+// import ShowPosts from './ShowPosts/ShowPosts'
+// import ShowComments from './ShowComments/ShowComments'
+// import DeletePost from './DeletePost/DeletePost'
+
+export default function Main() {
+
+    return (
+        <main>
+
+
+        </main>
+    )
+}
