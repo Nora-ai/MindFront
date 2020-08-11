5c2006655bdaf988afff3d05c8f97bc098a51267
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-08-11 17:03:08 -0400

    updated authorize request in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :update, :destroy]
+  before_action :authorize_request, only: [:create, :update, :destroy, :add_comment]
 
   # GET /posts
   def index
