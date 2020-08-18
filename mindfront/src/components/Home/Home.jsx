8953024ff9e12c06f48ea77a864152028768980e
Ellenora Tulchina <snora@MacBook-Pro.home>
2020-08-18 17:26:40 -0400

    mobile media query parially done

--- mindfront/src/components/Home/Home.jsx
+++ mindfront/src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home () {
         <div className="homepage-description">
             <h2>What is MindFront?</h2>
             <p className="mindfront-description">MindFront is a safe space for confrontation, for people to share anonymously yet mindfully about people, places and things in a positive light, even if the topic is sensitive. Mindfront will push us to mindfully confront each other and help us learn how to show up in this world with care and compassion.</p>
-            <p className='about-us'>About us</p>
+            <p className='about-us'>About Us</p>
         </div>
 
         <img className="homepage-image" src="https://res.cloudinary.com/alienora/image/upload/v1597189947/pexels-fotografierende-4617223_t7zr9m.jpg" alt="homepage-image"></img>
