1f1b3350057430932b5f5dbfe2afcaed9484513f
snora <noratulchina@gmail.com>
2021-03-14 17:15:17 -0400

    homepage edits

--- mindfront/src/components/Home/Home.jsx
+++ mindfront/src/components/Home/Home.jsx
@@ -8,6 +8,7 @@ export default function Home () {
 
     return (<>
 
+        <Video />
         <div className="homepage-description">
             <h2>What is MindFront?</h2>
             <p className="mindfront-description">MindFront is a safe space for confrontation, for people to share anonymously yet mindfully about people, places and things in a positive light. Mindfront will push us to mindfully confront each other and help us learn how to show up in this world with care and compassion. <i>Choose your words carefully.</i></p>
@@ -17,7 +18,7 @@ export default function Home () {
         {/* <img className="homepage-image" src="https://res.cloudinary.com/alienora/image/upload/v1597189947/pexels-fotografierende-4617223_t7zr9m.jpg" alt="homepage"></img> */}
 
         <div className="video-image">
-        <Video />
+        
         </div>
         <ShowPosts />
 
